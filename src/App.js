85242359d3375a3d790edcf7018ccba3f079fdf4
Rana_awais <z.manzoor@pioneerlogics.com>
2022-06-13 04:15:27 -0700

    seprate component for expenseData array

--- src/App.js
+++ src/App.js
@@ -1,34 +1,10 @@
 import React, { useState } from "react";
 import Expenses from "./Components/Expenses/Expenses";
 import NewExpense from "./Components/NewExpense/NewExpense";
-const expenses = [
-  {
-    id: "e1",
-    title: "Toilet Paper",
-    amount: 84.99,
-    date: new Date(2020, 7, 14),
-  },
-  {
-    id: "e2",
-    title: "New TV",
-    amount: 840.99,
-    date: new Date(2022, 7, 20),
-  },
-  {
-    id: "e3",
-    title: "Car Insurance",
-    amount: 220.99,
-    date: new Date(2020, 7, 14),
-  },
-  {
-    id: "e4",
-    title: "New Desk",
-    amount: 29.99,
-    date: new Date(2019, 5, 14),
-  },
-];
+import expenseData from "./Components/Expenses/expenseData";
+
 function App() {
-  const [enteredExpense, setExpense] = useState(expenses);
+  const [enteredExpense, setExpense] = useState(expenseData);
   const addExpenseHandler = (expense) => {
     setExpense((prevState) => {
       return [...prevState, expense];
---
+++ src/Components/Expenses/expenseData.js
@@ -0,0 +1,30 @@
+import React from "react";
+
+const expenseData = [
+  {
+    id: "e1",
+    title: "Toilet Paper",
+    amount: 84.99,
+    date: new Date(2020, 7, 14),
+  },
+  {
+    id: "e2",
+    title: "New TV",
+    amount: 840.99,
+    date: new Date(2022, 7, 20),
+  },
+  {
+    id: "e3",
+    title: "Car Insurance",
+    amount: 220.99,
+    date: new Date(2020, 7, 14),
+  },
+  {
+    id: "e4",
+    title: "New Desk",
+    amount: 29.99,
+    date: new Date(2019, 5, 14),
+  },
+];
+
+export default expenseData;
